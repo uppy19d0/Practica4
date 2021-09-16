5b4581d617fa7683e7faad66fa6008e9c417218d
Luis A.Tavarez <51054204+uppy19d0@users.noreply.github.com>
2021-09-16 11:33:34 -0400

    Update Program.cs

--- practica4/Program.cs
+++ practica4/Program.cs
@@ -6,7 +6,7 @@ namespace practica4
     class Program
     {
        
-        //2 -  Escriba los siguiente método como expresión lambda:
+   
 
         delegate int isODD(int number);
         delegate double computeITBIS(double price);
@@ -15,7 +15,11 @@ namespace practica4
         static void Main(string[] args)
         {
 
-            
+         //1- Hacer un método extendido que opere sobre el tipo List<int> llamado Pares()
+        //que retorne un List<int> con los valores pares en la lista asociada dada. En la implementación del método Pares() utilice el método
+        //FindAll() de las listas para pasarle una función predicado 
+        //mediante la implementación de un delegado que represente métodos que retornen bool y que reciban un entero como parámetro.
+
             List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10};
 
             foreach(int number in Helper.Pares(numbers))
@@ -24,7 +28,7 @@ namespace practica4
 
             }
 
-
+        /////////////////////////////////////////////////
 
 
 
